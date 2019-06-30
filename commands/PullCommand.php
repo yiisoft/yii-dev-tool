3f7275263e75c90a0726d39fd2c0be1c9b21f253
Alexander Makarov <sam@rmcreative.ru>
2019-06-30 19:47:36 +0300

    Add pull command

---
+++ commands/PullCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace yiidev\commands;
+
+class PullCommand
+{
+    private $package;
+
+    // TODO implement setting these
+    public $baseDir = __DIR__ . '/../dev';
+
+    public function __construct(string $package = null)
+    {
+        $this->package = $package;
+    }
+
+    public function run(): void
+    {
+        $packages = require __DIR__ . '/../packages.php';
+
+        if ($this->package === null) {
+            // install all packages
+            foreach ($packages as $p => $dir) {
+                $targetPath = $this->baseDir . DIRECTORY_SEPARATOR . $dir;
+                $this->pull($p, $targetPath);
+            }
+        } elseif (isset($packages[$this->package])) {
+            $targetPath = $this->baseDir . DIRECTORY_SEPARATOR . $packages[$this->package];
+            $this->pull($this->package, $targetPath);
+        } else {
+            stderrln("Package '$this->package' not found in packages.php");
+            exit(1);
+        }
+    }
+
+    private function pull(string $package, string $targetPath): void
+    {
+        stdoutln($package, 32);
+        $command = 'cd ' . escapeshellarg($targetPath) . ' && git pull';
+        $output = trim(shell_exec($command));
+
+        if (!empty($output)) {
+            echo $output . "\n\n";
+        }
+    }
+}
