ce0672a5cb4c2e0406fd27bc6b9d2c1081fd28f2
Alexander Makarov <sam@rmcreative.ru>
2019-07-02 14:29:05 +0300

    Add FUNDING.yml to replicate config

--- replicate.php
+++ replicate.php
@@ -5,6 +5,7 @@ return [
     'sourceFiles' => [
         '.github/ISSUE_TEMPLATE.md',
         '.github/PULL_REQUEST_TEMPLATE.md',
+        '.github/FUNDING.yml',
 
         '.scrutinizer.yml',
         '.styleci.yml',
