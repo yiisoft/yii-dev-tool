58584200fb177d33ff4a63c78c7caafa3af938df
Alexander Makarov <sam@rmcreative.ru>
2019-07-18 11:23:09 +0300

    Replicate CONTRIBUTING.md

--- replicate.php
+++ replicate.php
@@ -6,6 +6,7 @@ return [
         '.github/ISSUE_TEMPLATE.md',
         '.github/PULL_REQUEST_TEMPLATE.md',
         '.github/FUNDING.yml',
+        '.github/CONTRIBUTING.yml',
 
         '.scrutinizer.yml',
         '.styleci.yml',
