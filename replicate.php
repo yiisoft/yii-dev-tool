e024131f36f646c6361b15329c0f3be53b86aa64
Alexander Makarov <sam@rmcreative.ru>
2019-09-08 23:43:38 +0300

    Update replication file list

--- replicate.php
+++ replicate.php
@@ -6,7 +6,9 @@ return [
         '.github/ISSUE_TEMPLATE.md',
         '.github/PULL_REQUEST_TEMPLATE.md',
         '.github/FUNDING.yml',
-        '.github/CONTRIBUTING.yml',
+        '.github/CONTRIBUTING.md',
+        '.github/CODE_OF_CONDUCT.md',
+        '.github/SECURITY.md',
 
         '.scrutinizer.yml',
         '.styleci.yml',
