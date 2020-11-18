78b8a8cffd1260f798f5f30607157d8f6c53bb79
Alexander Makarov <sam@rmcreative.ru>
2020-11-18 19:36:54 +0300

    Enhance replicate/files to be way more flexible

--- config/replicate/files.php
+++ config/replicate/files.php
@@ -1,22 +1,147 @@
 <?php
-
-// replication config
+/**
+ * Replication config.
+ */
 return [
-    'sourcePackage' => 'package-template',
-    'sourceFiles' => [
-        '.github/ISSUE_TEMPLATE.md',
-        '.github/PULL_REQUEST_TEMPLATE.md',
-        '.github/FUNDING.yml',
-        '.github/CONTRIBUTING.md',
-        '.github/CODE_OF_CONDUCT.md',
-        '.github/SECURITY.md',
-        '.github/workflows/build.yml',
+    'common' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [],
+        ],
+        'files' => [
+            '.github/CODE_OF_CONDUCT.md',
+            '.github/CONTRIBUTING.md',
+            '.github/ISSUE_TEMPLATE.md',
+            '.github/PULL_REQUEST_TEMPLATE.md',
+            '.github/SECURITY.md',
+            '.github/FUNDING.yml',
+            '.editorconfig',
+            'LICENSE.md',
+        ],
+    ],
+    'gitattributes' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
+            ],
+        ],
+        'files' => [
+            '.gitattributes',
+        ],
+    ],
+    'dependabot' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
+            ],
+        ],
+        'files' => [
+            '.github/dependabot.yml',
+        ],
+    ],
+    'scrutinizer' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
+                'access',
+                // apps (they have to report coverage from Codeception)
+                'app',
+                'app-api',
+                'yii-demo',
+            ],
+        ],
+        'files' => [
+            '.scrutinizer.yml'
+        ],
+    ],
+    'styleci' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
+                'requirements'
+            ],
+        ],
+        'files' => [
+            '.styleci.yml'
+        ],
+    ],
+    'bc_' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
+                'access',
+                'aliases',
+                'http',
+                'json',
+                'security',
+                'strings',
+                'auth',
+                'friendly-exception',
+                'injector',
+                'requirements',
 
-        '.phan/config.php',
+                // apps
+                'app',
+                'app-api',
+                'yii-demo',
+            ],
+        ],
+        'files' => [
+            '.github/workflows/bc.yml',
+        ],
+    ],
+    'build' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
+            ],
+        ],
+        'files' => [
+            '.github/workflows/build.yml',
+        ],
+    ],
+    'mutation' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
 
-        '.editorconfig',
-        '.scrutinizer.yml',
-        '.styleci.yml',
-        '.gitattributes',
+                // apps
+                'app',
+                'app-api',
+                'yii-demo',
+            ],
+        ],
+        'files' => [
+            '.github/workflows/mutation.yml',
+            'infection.json.dist',
+        ],
+    ],
+    'static' => [
+        'source' => 'package-template',
+        'packages' => [
+            'include' => ['*'],
+            'exclude' => [
+                'docs',
+            ],
+        ],
+        'files' => [
+            '.github/workflows/static.yml',
+            'psalm.xml',
+        ],
     ],
 ];
